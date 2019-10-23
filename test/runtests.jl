799b48f35c48b811943ded5c8bed2cb0ed31db5d
UnofficialJuliaMirrorBot <43731649+UnofficialJuliaMirrorBot@users.noreply.github.com>
2019-10-23 14:14:16 +0000

    Snapshot of branch master taken on 2019-10-23T10:14:14.943-04:00 from "https://github.com/UnofficialJuliaMirror/Mortar2D.jl-0b7cd5e2-b3f3-5fae-9080-a65912ed73e3"

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,24 +1,12 @@
 # This file is a part of JuliaFEM.
 # License is MIT: see https://github.com/JuliaFEM/Mortar2D.jl/blob/master/LICENSE
 
-using Base.Test
-using TimerOutputs
-const to = TimerOutput()
-
-test_files = String[]
-push!(test_files, "test_calculate_normals.jl")
-push!(test_files, "test_calculate_projections.jl")
-push!(test_files, "test_calculate_segments.jl")
-push!(test_files, "test_calculate_mortar_matrices.jl")
-push!(test_files, "test_calculate_mortar_assembly.jl")
+using Test
 
 @testset "Mortar2D.jl" begin
-    for fn in test_files
-        timeit(to, fn) do
-            include(fn)
-        end
-    end
+    @testset "test_calculate_normals.jl" begin include("test_calculate_normals.jl") end
+    @testset "test_calculate_projections.jl" begin include("test_calculate_projections.jl") end
+    @testset "test_calculate_segments.jl" begin include("test_calculate_segments.jl") end
+    @testset "test_calculate_mortar_matrices.jl" begin include("test_calculate_mortar_matrices.jl") end
+    @testset "test_calculate_mortar_assembly.jl" begin include("test_calculate_mortar_assembly.jl") end
 end
-println()
-println("Test statistics:")
-println(to)
